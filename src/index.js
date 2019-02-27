cf589a014ad4894aedbc0d7b685d0da3bda7396c
Yury Misiyevich <vinchesterok@tut.by>
2019-02-27 15:23:16 +0300

    feat: add function body for searching love triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+  let count=0;
+  for(let a=1;a<=preferences.length;a++){
+      for(let b=a+1;b<=preferences.length;b++){
+          for(let c=a+1;c<=preferences.length;c++){
+              if(a===preferences[c-1] && preferences[a-1]===b && preferences[b-1]===c){
+                  count++;
+              }
+          }
+      }
+  }
+  return count;
 };
